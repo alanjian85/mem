e68d088eb9263636761289b8a6ebbcf79932f104
Alan Jian <alanjian85@outlook.com>
2023-01-20 23:48:26 +0800

    Add example code of usage

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "mem.h"
+
+#define HEAP_SIZE 4096
+
+typedef char byte;
+
+static byte heap[HEAP_SIZE];
+
+int main() {
+    mem_init(heap, HEAP_SIZE);
+    size_t *size = mem_alloc(sizeof(size_t));
+    *size = HEAP_SIZE;
+    printf("Heap size: %llu\n", size);
+    mem_free(size);
+    return 0;
+}
