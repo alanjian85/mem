28fd77efd0d6b586a93d0d1bf8741012f0324045
Alan Jian <alanjian85@outlook.com>
2023-01-21 15:49:47 +0800

    Implement mem_alloc and mem_free

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
     mem_init(heap, HEAP_SIZE);
     size_t *size = mem_alloc(sizeof(size_t));
     *size = HEAP_SIZE;
-    printf("Heap size: %llu\n", size);
+    printf("Heap size: %llu\n", *size);
     mem_free(size);
     return 0;
 }
--- mem.c
+++ mem.c
@@ -1,16 +1,48 @@
 #include "mem.h"
 
+#include <stdbool.h>
+
 #include "assert.h"
 
-static byte *heap_start;
-static byte *heap_end;
+static void *heap_start, *heap_end;
+
+size_t align(size_t x, size_t n) {
+    size_t rem = x % n;
+    if (rem != 0)
+        x += n - rem;
+    return x;
+}
 
 void mem_init(void *heap, size_t size) {
-    assert(size & 0x1 == 0);
+    assert((size & 0x1) == 0);
 
-    heap_start = (byte *) heap;
+    heap_start = heap;
     heap_end = heap_start + size;
 
-    *heap_start = size | 0x0;
-    *(heap_end - 1) = *heap_start;
+    *((size_t *) heap_start) = size | 0x0;
+    *((size_t *) heap_end - sizeof(size_t)) = size;
+}
+
+void *mem_alloc(size_t size) {
+    size += 2 * sizeof(size_t);
+    size = align(size, 16);
+    void *ptr = heap_start;
+    size_t seg_size = 0;
+    bool allocated;
+    do {
+        ptr += seg_size;
+        if (ptr >= heap_end)
+            return NULL;
+        seg_size = *((size_t *) ptr) & ~0x1;
+        allocated = *((size_t *) ptr) & 0x1;
+    } while (seg_size < size || allocated);
+    *((size_t *) ptr) = size | 0x1;
+    *((size_t *) ptr + size - sizeof(size_t)) = size;
+    return ptr + 1;
+}
+
+void mem_free(void *ptr) {
+    --ptr;
+    assert(*((size_t *) ptr) & 0x1);
+    *((size_t *) ptr) &= ~0x1;
 }
