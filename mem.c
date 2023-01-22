f8e968b1dc04f46463a7ba6132bc04f3e75189e7
Alan Jian <alanjian85@outlook.com>
2023-01-22 12:46:49 +0800

    Remove unnecessary pointer conversions

--- mem.c
+++ mem.c
@@ -16,8 +16,8 @@ static inline size_t align(size_t x, size_t n) {
 void mem_init(void *heap, size_t size) {
     assert((size & 0x1) == 0);
 
-    heap_start = (size_t *) heap;
-    heap_end = (size_t *) (heap + size);
+    heap_start = heap;
+    heap_end = heap + size;
 
     *heap_start = size | 0x0;
     *(heap_end - 1) = size | 0x0;
@@ -30,7 +30,7 @@ void *mem_alloc(size_t size) {
     size_t block_size = 0;
     bool allocated;
     do {
-        ptr = (size_t *) ((void *) ptr + block_size);
+        ptr = (void *) ptr + block_size;
         if (ptr >= heap_end)
             return NULL;
         block_size = *ptr & ~0x1;
@@ -41,7 +41,7 @@ void *mem_alloc(size_t size) {
         size += block_size;
         block_size = 0;
     } 
-    size_t *end = (size_t *) ((void *) ptr + size);
+    size_t *end = (void *) ptr + size;
     *ptr = size | 0x1;
     *(end - 1) = size | 0x1;
     if (block_size > 0) {
@@ -52,11 +52,11 @@ void *mem_alloc(size_t size) {
 }
 
 void mem_free(void *ptr) {
-    size_t *block = (size_t *) (ptr - sizeof(size_t));
+    size_t *block = ptr - sizeof(size_t);
     assert(*block & 0x1);
     *block &= ~0x1;
     size_t size = *block;
-    size_t *end = (size_t *) ((void *) block + size);
+    size_t *end = (void *) block + size;
     *(end - 1) &= ~0x1;    
 
     size_t prev_tag = *(block - 1);
