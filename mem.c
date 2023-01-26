b24e19e9b0c446336c75f78cf3da7fc33d51670a
Alan Jian <alanjian85@outlook.com>
2023-01-26 15:32:48 +0800

    Use uintptr_t instead of void* to cast pointers

--- mem.c
+++ mem.c
@@ -1,6 +1,7 @@
 #include "mem.h"
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "assert.h"
 
@@ -17,8 +18,8 @@ static inline size_t align(size_t x, size_t n) {
 void mem_init(void *heap, size_t size) {
     assert((size & 0x1) == 0);
 
-    heap_start = (size_t *) align((size_t) heap + 1, 16) - 1;
-    heap_end = heap + size;
+    heap_start = (size_t *) (align((size_t) heap + 1, 16) - sizeof(size_t));
+    heap_end = (size_t *) ((uintptr_t) heap + size);
 
     *heap_start = size | 0x0;
     *(heap_end - 1) = size | 0x0;
@@ -29,49 +30,50 @@ void *mem_alloc(size_t size) {
     size = align(size, 16);
     size_t *ptr = heap_start;
     size_t block_size;
+    // first-fit
     while (ptr < heap_end) {
         block_size = *ptr & ~0x1;
         bool allocated = *ptr & 0x1;
         if (block_size >= size && !allocated)
             break;
-        ptr = (void *) ptr + block_size;
+        ptr = (size_t *) ((uintptr_t) ptr + block_size);
     }
     if (ptr >= heap_end)
         return NULL;
     block_size -= size;
-    if (block_size < sizeof(size_t)) {
+    if (block_size < sizeof(size_t) << 1) {
         size += block_size;
         block_size = 0;
     } 
-    size_t *end = (void *) ptr + size;
+    size_t *end = (size_t *) ((uintptr_t) ptr + size);
     *ptr = size | 0x1;
     *(end - 1) = size | 0x1;
     if (block_size > 0) {
         *end = block_size | 0x0;
-        *((size_t *) ((void *) end + block_size) - 1) = block_size | 0x0;
+        *((size_t *) ((uintptr_t) end + block_size - sizeof(size_t))) = block_size | 0x0;
     }
     return ptr + 1;
 }
 
 void mem_free(void *ptr) {
-    size_t *block = ptr - sizeof(size_t);
+    size_t *block = (size_t *) ((uintptr_t) ptr - sizeof(size_t));
     assert(*block & 0x1);
     *block &= ~0x1;
     size_t size = *block;
-    size_t *end = (void *) block + size;
+    size_t *end = (size_t *) ((uintptr_t) block + size);
     *(end - 1) &= ~0x1;    
 
     size_t prev_tag = *(block - 1);
     if ((prev_tag & 0x1) == 0x0) {
         size += prev_tag;
-        block = (void *) block - prev_tag;
+        block = (size_t *) ((uintptr_t) block - prev_tag);
         *block = *(end - 1) = size; 
     }
 
     size_t next_tag = *end;
     if ((next_tag & 0x1) == 0x0) {
         size += next_tag;
-        end = (void *) end + next_tag;
+        end = (size_t *) ((uintptr_t) end + next_tag);
         *block = *(end - 1) = size;
     }
 }
